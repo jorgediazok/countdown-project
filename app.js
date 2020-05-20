19477ae66f37436429a30c9de222c86bdcfcb6b6
Jorge <jorgediazok@gmail.com>
2020-05-20 20:30:00 -0300

    date completed

--- app.js
+++ app.js
@@ -26,10 +26,16 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2020, 4, 24, 11, 30, 0);
-console.log(futureDate);
+let futureDate = new Date(2020, 3, 25, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
 
-giveaway.textContent = `giveaway ends on ${year}`;
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`;
