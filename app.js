bf6e761c057ad59e8e78878d204b6101a614c654
Jorge <jorgediazok@gmail.com>
2020-05-20 20:05:06 -0300

    adding date

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2020, 4, 24, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+
+giveaway.textContent = `giveaway ends on ${year}`;
