fed993e9470a9c8acb4e5dfe15bf2f5e11c7e0ae
Jorge <jorgediazok@gmail.com>
2020-05-20 21:04:40 -0300

    panel

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2020, 3, 25, 11, 30, 0);
+let futureDate = new Date(2020, 4, 25, 15, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -39,3 +39,28 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+//Future time in miliseconds
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  //values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  //calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+  //set values array
+  const values = [days, hours, minutes, seconds];
+  items.forEach(function (item, index) {
+    item.innerHTML = values[index];
+  });
+}
+
+getRemainingTime();
