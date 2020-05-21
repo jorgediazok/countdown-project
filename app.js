2dcfe66e9c3a7bad6ee532108b38f797a8e709f4
Jorge <jorgediazok@gmail.com>
2020-05-20 21:33:21 -0300

    clock working

--- app.js
+++ app.js
@@ -58,9 +58,21 @@ function getRemainingTime() {
   let seconds = Math.floor((t % oneMinute) / 1000);
   //set values array
   const values = [days, hours, minutes, seconds];
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
   items.forEach(function (item, index) {
-    item.innerHTML = values[index];
+    item.innerHTML = format(values[index]);
   });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired!</h4>`;
+  }
 }
+//countdown
+let countdown = setInterval(getRemainingTime, 1000);
 
 getRemainingTime();
